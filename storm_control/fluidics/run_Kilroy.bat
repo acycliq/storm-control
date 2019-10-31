2295751aeadcda1924e9c7893717bc4f836f6f0f
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2019-10-31 14:50:38 +0000

    minor change

--- storm_control/fluidics/run_Kilroy.bat
+++ storm_control/fluidics/run_Kilroy.bat
@@ -5,6 +5,7 @@ SET ROOT="C:\Users\cortexlab\Anaconda3"
 SET EXE="C:\Users\cortexlab\Anaconda3\envs\storm-control\python.exe"
 SET KILROY="C:\dev\Python\storm-control\storm_control\fluidics\kilroy.py"
 SET PYTHONPATH=%PYTHONPATH%;C:\dev\Python\storm-control
+
 CALL %ROOT%\Scripts\activate.bat storm-control
 %EXE% %KILROY% %XML%
 
