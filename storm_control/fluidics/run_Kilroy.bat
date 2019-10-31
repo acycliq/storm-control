da757da280224b8fcf647222cd59ce1e108633ca
Dimitris Nicoloutsopoulos <dimitris.nicoloutsopoulos@gmail.com>
2019-10-31 14:49:44 +0000

    updated batch file

--- storm_control/fluidics/run_Kilroy.bat
+++ storm_control/fluidics/run_Kilroy.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
+SET XML="C:\dev\Python\storm-control\storm_control\fluidics\kilroy_settings_default.xml"
 
-SET EXE="C:\Users\experiment\Miniconda3\envs\storm-control\python.exe"
-SET KILROY="D:\Dimitris\OneDrive - University College London\dev\Python\storm-control\storm_control\fluidics\kilroy.py"
-SET XML="D:\Dimitris\OneDrive - University College London\dev\Python\storm-control\storm_control\fluidics\kilroy_settings_default.xml"
-
+SET ROOT="C:\Users\cortexlab\Anaconda3"
+SET EXE="C:\Users\cortexlab\Anaconda3\envs\storm-control\python.exe"
+SET KILROY="C:\dev\Python\storm-control\storm_control\fluidics\kilroy.py"
+SET PYTHONPATH=%PYTHONPATH%;C:\dev\Python\storm-control
+CALL %ROOT%\Scripts\activate.bat storm-control
 %EXE% %KILROY% %XML%
 
-REM PAUSE
+PAUSE
